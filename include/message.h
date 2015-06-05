1979eecd161e0baa89deb51e590b6ec3eddc2f40
Lucas Mello <chapeutyler@gmail.com>
2015-06-05 01:34:01 -0300

    Message finished?

--- include/message.h
+++ include/message.h
@@ -3,7 +3,19 @@
 
 #include "contact.h"
 
+#define MESSAGE_MAXSIZE 519
+
+/*
+ * Envia uma mensagem já codificada utilizando json
+ * para um contato especifico
+*/
 int message_send(contact *receiver, char *send_data);
+
+/*
+ * Thread responsavel por receber mensagens de outros
+ * contatos
+ * (void*)data é do tipo contact*
+*/
 void *message_receive(void *data);
 
 #endif //_MESSAGE_H_
--- src/message.c
+++ src/message.c
@@ -5,41 +5,62 @@
 #include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "contact.h"
 #include "message.h"
 #include "global.h"
+#include "jsonxstr.h"
 
-#define MESSAGE_MAXSIZE 519
-
+/*
+ * Envia uma mensagem já codificada utilizando json
+ * para um contato especifico
+*/
 int message_send(contact *receiver, char *send_data)
 {
+    // Se foi passado um contato inválido
     if(receiver == 0)
     {
         perror("Receiver doesn't exist");
         return -1;
     }
 
+    // Envia a mensagem
     send(receiver->socketvar, send_data, strlen(send_data), 0);
 
     return 0;
 }
 
+/*
+ * Thread responsavel por receber mensagens de outros
+ * contatos
+ * (void*)data é do tipo contact*
+*/
 void *message_receive(void *data)
 {
     contact *sender = (contact*)data;
 
+    // Quantidade de bytes recebidos
     int bytes_recv;
+    // Dados recebidos
     char recv_data[MESSAGE_MAXSIZE];
+    // Dados incompletos do ultimo recv que estao esperando o restante da mensagem
+    // Tambem armazena a concatenacao desses dados com os do recv atual
+    char cat_message[2 * MESSAGE_MAXSIZE];
 
+    cat_message[0] = '\0';
+
+    // Enquanto o programa nao for fechado
     while(running)
     {
+        // Fecha o socket e finaliza o thread se o contato estiver desconectado
         if(sender->status == STATUS_DEAD)
         {
             close(sender->socketvar);
             pthread_exit(0);
         }
 
+        // Recebe os dados
         bytes_recv = recv(sender->socketvar, recv_data, MESSAGE_MAXSIZE, 0);
 
         // Se ocorreu timeout, checa se o cliente pode estar disconectado
@@ -51,14 +72,53 @@ void *message_receive(void *data)
             if( (time(NULL) - sender->lastUpdate) > 40)
             {
                 sender->status = STATUS_DEAD; // O nó disconectou
-                close(sender->socketvar);
+                close(sender->socketvar); // Fecha o socket
                 pthread_exit(0);
             }
         }
         else
         {
+            // Armazena quantos caracteres contem a mensagem json valida
+            int length;
+            // Armazena uma mensagem json valida
+            char *json_message;
+            // Usado como auxiliar para validar as mensagens json
+            char *aux_recvData;
+
+            // Tranforma os dados recebidos em uma string valida
             recv_data[bytes_recv] = '\0';
-            // TODO funcao magica e tals
+            // Concatena a mensagem incompleta do recv anterior com os dados do recv atual
+            strcat(cat_message, recv_data);
+
+            // Comeca apontado para a primeira posicao de cat_message
+            aux_recvData = cat_message;
+
+            do
+            {
+                // Obtem uma mensagem json valida
+                json_message = validateJSON(aux_recvData, &length);
+                
+                // Se a mensagem não for valida ou chegou ao fim da string
+                if(json_message == NULL)
+                {
+                    // Copia para cat_message
+                    strcpy(cat_message, aux_recvData);
+                }
+                else
+                {
+                    // Desloca para a proxima mensagem
+                    aux_recvData += length + 1;
+                    // Se ultrapassou o limite do buffer faz apontar para o final dele
+                    if( (aux_recvData - (cat_message + strlen(cat_message))) >= 0)
+                        aux_recvData = cat_message + strlen(cat_message);
+
+                    // Transforma a mensagem json em uma mensagem normal
+                    decodeJSON(json_message);
+
+                    free(json_message);
+                }
+            }
+            while(json_message != NULL);
         }
 
         sleep(1);
